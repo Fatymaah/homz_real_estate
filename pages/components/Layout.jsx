0c414654362879afaaa0b29806beeeb932c6553e
Fatymaah <fatymahmohamed@gmail.com>
2022-01-29 17:10:24 +0300

    real estate final

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -10,14 +10,12 @@ const Layout = ({ children }) => {
  return <>
   <Head>
       <title>Homz</title>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
   </Head>
   <Box maxWidth='1280px' m='auto'>
       <header>
           <Navbar/>
       </header>
         <main>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
        {children}
         </main>
         <Footer>
