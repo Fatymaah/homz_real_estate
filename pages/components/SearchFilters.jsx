0e4af63c0eb73bc18bcbf1cd2a90b9d5d8d845bd
Fatma Mohamed <35401881+Fatymaah@users.noreply.github.com>
2022-02-02 08:34:51 +0300

    updated search

--- pages/components/SearchFilters.jsx
+++ pages/components/SearchFilters.jsx
@@ -100,7 +100,7 @@ export default function SearchFilters() {
                 ))}
                 {!loading && !locationData?.length && (
                   <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                    <Image src={noresult} width={100} height={100}/>
+                    <Image src={noresult} width={100} height={100} alt="noresult"/>
                     <Text fontSize='xl' marginTop='3'>
                       Waiting to search!
                     </Text>
